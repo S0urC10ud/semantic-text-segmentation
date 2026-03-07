a2b0d4674e98e19df0999e0aa56502a0b75df0c0
Martin Dallinger <martin.dallinger@nx-ai.com>
2026-03-07 10:28:12 +0100

    fix: change static_argnums index to 2 to fix tracer bool bug in mamba remat

--- train/utils/model.py
+++ train/utils/model.py
@@ -264,7 +264,7 @@ class Mamba1D(nn.Module):
             h = nn.Dropout(rate=float(self.dropout_rate), deterministic=not train)(h)
 
         for _ in range(int(self.n_layers)):
-            h = nn.remat(MambaBlock1D, static_argnums=(1,))(
+            h = nn.remat(MambaBlock1D, static_argnums=(2,))(
                 d_model=int(self.d_model),
                 d_state=int(self.d_state),
                 expand=int(self.expand),
